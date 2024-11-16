b73fca1d429767958e144b39df61ce8a083c88a9
Yogesh <iyogeshjangra@gmail.com>
2024-11-16 20:24:45 +0530

    Final Changes

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,7 +6,6 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -85,7 +84,6 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
-        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don't have an account?</p>
